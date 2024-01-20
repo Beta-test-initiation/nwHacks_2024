cdae490c4521c174f4bf278cd9d88d528897bbc0
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-01-20 12:02:35 -0800

    next-ui setup

--- explore_able/app/page.js
+++ explore_able/app/page.js
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        Hello
-      </div>
-
-      
-
-      
-          
-        
-          
+        <Button className="text-white">Hi</Button>
+      </div>   
     </main>
   );
 }
