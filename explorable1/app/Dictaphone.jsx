6cbd4169777b58434b92beacd8c70ecea701d2bf
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-01-21 06:39:57 -0800

    yayyy  dictaphone works

--- explorable1/app/Dictaphone.jsx
+++ explorable1/app/Dictaphone.jsx
@@ -1,30 +1,30 @@
 
-// import React, { useEffect } from 'react';
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import React, { useEffect } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-// const Dictaphone = () => {
-//   const {
-//     transcript,
-//     listening,
-//     resetTranscript,
-//     browserSupportsSpeechRecognition
-//   } = useSpeechRecognition();
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
 
-//   useEffect(() => {
-//     if (browserSupportsSpeechRecognition) {
-//       SpeechRecognition.startListening({ continuous: true });
-//     }
-//   }, [browserSupportsSpeechRecognition]);
+  useEffect(() => {
+    if (browserSupportsSpeechRecognition) {
+      SpeechRecognition.startListening({ continuous: true });
+    }
+  }, [browserSupportsSpeechRecognition]);
 
-//   if (!browserSupportsSpeechRecognition) {
-//     return <span>Browser doesn't support speech recognition.</span>;
-//   }
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
 
-//   return (
-//     <div>
-//       <p>{transcript}</p>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <p>{transcript}</p>
+    </div>
+  );
+};
 
-// export default Dictaphone;
+export default Dictaphone;
