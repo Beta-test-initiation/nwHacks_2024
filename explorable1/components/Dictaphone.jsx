81b55a4499a8f8840eef4ec889ab0d81a543c53b
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2024-01-21 00:56:17 -0800

    Update Dictaphone.jsx

--- explorable1/components/Dictaphone.jsx
+++ explorable1/components/Dictaphone.jsx
@@ -22,8 +22,7 @@ const Dictaphone = () => {
   return (
     <div>
       <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={() => SpeechRecognition.stopListening()}>Stop</button>
-      <button onClick={() => resetTranscript()}>Reset</button>
+
       <p>{transcript}</p>
     </div>
   );
